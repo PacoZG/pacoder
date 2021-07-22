467e12385762a6e09e12aac2531c954a82e6a2ea
Francisco Zavala <pako.zavala@gmail.com>
2021-07-22 03:11:18 +0300

    Last update of the day 22.07

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -7,12 +7,12 @@ const Presentation = () => {
     <div className="flex flex-col items-center md:flex-row md:justify-center bg-gray-300 dark:bg-gray-900 p-10 w-screen transition duration-300">
       <div className="flex flex-col items-center md:pl-20">
         <img
-          className=" w-2/3 h-2/3 md:h-max md:w-max border-4 border-black dark:border-gray-300 rounded-xl transition duration-300 "
+          className=" w-2/3 h-2/3 md:h-96 md:w-80 border-4 border-black dark:border-gray-300 rounded-xl transition duration-300 "
           src="https://res.cloudinary.com/dbn5gpgi5/image/upload/v1625155266/CV%20Pics/kxcpai1mniycdxszmgbq.jpg"
           alt="profile"
         />
       </div>
-      <div className="p-10 md:pt-16">
+      <div className="p-10 md:pt-10 w-3/4">
         <h1 className="text-2xl text-gray-700 dark:text-gray-300 text-right transition duration-300">
           {t('Presentation.line0')}
           <span className="text-blue-700 text-right">FRANCISCO ZAVALA GONZALEZ</span>
