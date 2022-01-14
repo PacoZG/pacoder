693deef28e3372ee70ff08a5a706a91bed6cb8c9
Francisco Zavala <pako.zavala@gmail.com>
2022-01-14 18:17:35 +0200

    Trying github workflows

---
+++ src/components/Health.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Health = () => {
+  return (
+    <div className="page-layout">
+      <h1 className=" text-white">ok</h1>
+    </div>
+  )
+}
+
+export default Health
