ecddd951a162cd39a2d0ecadadf9069155db1cb2
Francisco Zavala <pako.zavala@gmail.com>
2022-03-01 20:43:57 +0200

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
 import { useTranslation } from 'react-i18next'
 import { useField } from '../../hooks/InputHooks'
-import variableService from '../../services/variables'
+// import variableService from '../../services/variables'
 import { ReactComponent as Spiner } from '../../utils/assets/spiner.svg'
 import Button from '../private/Button'
 import Div from '../private/Div'
@@ -22,9 +22,9 @@ const Contact = () => {
   const message = useField('text')
 
   const getKey = async () => {
-    const key = await variableService.getSiteKey()
-    // setSiteKey('6Ld6fKYeAAAAAG4F6BreaLvyHeUL-tVtib6oU1Ej')
-    setSiteKey(key.value)
+    // const key = await variableService.getSiteKey()
+    setSiteKey('6Ld6fKYeAAAAAG4F6BreaLvyHeUL-tVtib6oU1Ej')
+    // setSiteKey(key.value)
   }
 
   useEffect(() => {
@@ -57,9 +57,11 @@ const Contact = () => {
 
   if (!siteKey) {
     return (
-      <Div className="justify-center items-center flex outline-none bg-gray-400 min-h-screen">
-        <Div className="flex flex-row space-x-1">
-          <Spiner className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-300" />
+      <Div className="page-layout lg:pt-40 transform duration-75">
+        <Div className="justify-center items-center flex outline-none bg-gray-400 min-h-screen">
+          <Div className="flex flex-row space-x-1">
+            <Spiner className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-300" />
+          </Div>
         </Div>
       </Div>
     )
