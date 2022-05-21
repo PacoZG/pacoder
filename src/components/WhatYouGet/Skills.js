68b89adc45c2e543965c55a62b640c99240af731
Francisco Zavala <pako.zavala@gmail.com>
2022-05-21 15:54:45 +0300

    Update Skills.js

--- src/components/WhatYouGet/Skills.js
+++ src/components/WhatYouGet/Skills.js
@@ -16,7 +16,7 @@ const Skills = () => {
     <Div className="flex flex-col items-center lg:gap-8 justify-center p-8 md:p-16 bg-gray-300 dark:bg-gray-900 transition duration-500 bg-opacity-70 dark:bg-opacity-70">
       <Div className="flex flex-col justify-center lg:space-x-28 border-double border-b-8 border-gray-700 dark:border-gray-500  pb-12 lg:w-10/12">
         <Div className="hidden lg:flex flex-col space-y-9">
-          <Div className="flex items-center justify-center gap-24">
+          <Div className="flex items-center justify-evenly gap-24">
             <Div className="flex flex-col items-center ">
               <Img
                 className="skill-logo bg-yellow-200 p-2 "
@@ -35,7 +35,7 @@ const Skills = () => {
             </Div>
             <Div className="flex flex-col items-center">
               <Img
-                className="skill-logo bg-white p-3 "
+                className="skill-logo bg-white p-2 "
                 src="https://img.icons8.com/color/96/000000/redux.png"
                 alt="React"
               />
@@ -50,14 +50,14 @@ const Skills = () => {
               <P className="text-black dark:text-pink-700 transition duration-500">React-Native</P>
             </Div>
           </Div>
-          <Div className="flex items-center justify-center gap-24">
+          <Div className="flex items-center justify-evenly gap-24">
             <Div className="flex flex-col items-center ">
-              <Typescript className=" skill-logo w-20 rounded-none  " />
+              <Typescript className=" skill-logo w-24 rounded-none  " />
               <P className="text-black dark:text-yellow-500 transition duration-500">Typescript</P>
             </Div>
             <Div className="flex flex-col items-center">
               <Img
-                className="skill-logo bg-gray-300 p-2 "
+                className="skill-logo bg-gray-300 p-2  "
                 src="https://img.icons8.com/color/96/000000/mongodb.png"
                 alt="React"
               />
@@ -80,22 +80,22 @@ const Skills = () => {
               <P className="text-black dark:text-green-400 transition duration-500">Node</P>
             </Div>
           </Div>
-          <Div className="flex items-center justify-center gap-24">
+          <Div className="flex items-center justify-evenly gap-24">
             <Div className="flex flex-col items-center">
-              <Img alt="tailwind-logo" src={Tailwind} className="skill-logo mb-4 h-20 w-36" />
+              <Img alt="tailwind-logo" src={Tailwind} className="skill-logo mb-4 h-20 w-28" />
               <P className="text-black dark:text-blue-300 transition duration-500">Tailwindcss</P>
             </Div>
             <Div className="flex flex-col items-center">
-              <Cypress className="skill-logo h-16 sm:h-20 md:h-24" />
+              <Cypress className="skill-logo h-24 " />
               <P className="text-black dark:text-gray-50 transition duration-500">Cypress.io</P>
             </Div>
             <Div className="flex flex-col items-center">
               <AWSLogo className="skill-logo p-3 w-24 h-24 bg-white" />
-              <P className=" text-center text-yellow-400 transition duration-500">AWS Junior Developer Use</P>
+              <P className=" text-center text-yellow-400 transition duration-500">Junior</P>
             </Div>
             <Div className="flex flex-col items-center">
               <DockerLogo className="skill-logo p-3 w-24 h-24 bg-gray-50 bg-opacity-10" />
-              <P className=" text-center text-blue-700 transition duration-500">DevOps with Docker</P>
+              <P className=" text-center text-blue-700 transition duration-500">Docker</P>
             </Div>
           </Div>
         </Div>
@@ -156,33 +156,37 @@ const Skills = () => {
               </Div>
             </Div>
 
-            <Div className="flex items-center justify-evenly space-x-5 sm:space-x-11 md:space-x-14">
+            <Div className="flex items-center justify-evenly space-x-9 sm:space-x-16 md:space-x-24">
               <Div className="flex flex-col items-center">
                 <Img
-                  className="skill-logo bg-green-400 p-2  "
+                  className="skill-logo bg-green-400 p-2 md:h-28 "
                   src="https://img.icons8.com/color/96/000000/nodejs.png"
                   alt="React"
                 />
                 <P className="text-sm text-black dark:text-green-400 transition duration-500">Node</P>
               </Div>
               <Div className="flex flex-col items-center">
-                <Img alt="tailwind-logo" src={Tailwind} className="skill-logo h-16 w-20 mb-4 pt-3" />
+                <Img alt="tailwind-logo" src={Tailwind} className="skill-logo h-20 w-24 mb-4 pt-3 md:h-20 md:w-24" />
                 <P className="text-sm text-black dark:text-blue-300 transition duration-500">Tailwindcss</P>
               </Div>
               <Div className="flex flex-col items-center">
-                <Cypress className="skill-logo h-20 w-20" />
+                <Cypress className="skill-logo h-20 w-20 md:h-24 md:w-24" />
                 <P className="text-sm text-black dark:text-gray-300 transition duration-500">Cypress.io</P>
               </Div>
             </Div>
 
-            <Div className="flex items-center justify-evenly space-x-5 sm:space-x-11 md:space-x-14">
+            <Div className="flex items-center justify-evenly space-x-9 sm:space-x-16 md:space-x-24">
               <Div className="flex flex-col items-center">
-                <Typescript className="skill-logo w-16 rounded-none" />
+                <Typescript className="skill-logo w-20 my-2 rounded-none md:h-24 md:w-24" />
                 <P className="text-sm text-black dark:text-green-400 transition duration-500">Typescript</P>
               </Div>
               <Div className="flex flex-col items-center">
-                <AWSLogo className="skill-logo p-2 my-2 h-16 w-16 bg-white" />
-                <P className=" text-yellow-400 transition duration-500">Junior Dev Use</P>
+                <AWSLogo className="skill-logo p-3 my-2 h-20 w-20 md:h-24 md:w-24 bg-white" />
+                <P className=" text-center text-yellow-400 transition duration-500">Junior</P>
+              </Div>
+              <Div className="flex flex-col items-center">
+                <DockerLogo className="skill-logo p-2 my-2 h-20 w-20 md:h-24 md:w-24 bg-gray-50 bg-opacity-10" />
+                <P className=" text-center text-blue-700 transition duration-500 w-16">Docker</P>
               </Div>
             </Div>
           </Div>
