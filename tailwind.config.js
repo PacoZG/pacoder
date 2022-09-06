81279e0790ba415390d90aab79312191fdb3127b
pacozavala <pako.zavala@gmail.com>
2022-09-06 17:12:58 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,6 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
@@ -278,7 +277,7 @@ module.exports = {
       ],
     },
     fontSize: {
-      xxs: ['0.70rem', { lineHeight: '0.75rem' }],
+      '2xs': ['0.70rem', { lineHeight: '0.75rem' }],
       xs: ['0.75rem', { lineHeight: '1rem' }],
       sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
