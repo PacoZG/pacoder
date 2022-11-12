2543bab93826127db23cb82147665380ad6143c8
pacozavala <pako.zavala@gmail.com>
2022-11-12 12:50:39 +0200

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -79,9 +79,9 @@ const Contact = () => {
 
         <Div className="flex flex-col md:flex-row items-center">
           <Div className="flex flex-col sm:flex-row items-center justify-center w-full">
-            <Div className="w-full xxs:w-28 xs:w-32 flex justify-center sm:justify-between pt-2 xs:pt-0">
-              {showModal ? (
-                <P className="text-md text-green-800 dark:text-green-300 transform duration-500 w-40">
+            <Div className="flex justify-center sm:justify-between pt-2 w-full">
+              {!showModal ? (
+                <P className="text-md text-center text-green-800 dark:text-green-300 transform duration-500">
                   {'Thanks / Kiitos / Gracias'}
                 </P>
               ) : (
